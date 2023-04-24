87903d01ee89861a36dd3f0997fd4bc4f7f866fb
Aaron <2klaus.4peter@gmail.com>
2023-04-24 17:56:57 +0200

    add unteste node methods

--- application/include/node.hpp
+++ application/include/node.hpp
@@ -4,29 +4,29 @@
 #include <glm/glm.hpp>
 #include <memory>
 #include <string>
-#include <vector>
+#include <map>
 
 class Node {
 public:
-  Node(std::string const& name, std::string const& path, std::shared_ptr<Node> parent);
+  Node(std::string const& name, std::shared_ptr<Node> parent);
 
   std::shared_ptr<Node> getParent();
   void setParent(std::shared_ptr<Node> node);
   std::shared_ptr<Node> getChild(std::string const& name);
-  std::vector<std::shared_ptr<Node>> const& getChildrenList();
+  std::map<std::string, std::shared_ptr<Node>> const& getChildrenList();
   std::string const& getName();
   std::string const& getPath();
   int getDepth();
   glm::mat4 getLocalTransform();
-  void setLocalTransform(glm::mat4 const& mat);
+  void setLocalTransform(glm::mat4 const& newTransform);
   glm::mat4 const& getWorldTransform();
-  void setWorldTransform(glm::mat4 const& mat);
+  void setWorldTransform(glm::mat4 const& newTransform);
   void addChildren(std::shared_ptr<Node>);
   std::shared_ptr<Node> removeChildren(std::string const& name);
 
 private:
   std::shared_ptr<Node> m_parent;
-  std::vector<std::shared_ptr<Node>> m_children;
+  std::map<std::string, std::shared_ptr<Node>> m_children;
   std::string m_name;
   std::string m_path;
   int m_depth;
--- application/source/node.cpp
+++ application/source/node.cpp
@@ -2,31 +2,42 @@
 
 
 Node::Node(std::string const& name, std::shared_ptr<Node> parent) :
-    m_name{name},
-    m_path{parent->getPath() + name},
     m_parent{parent},
-    m_depth{parent->getDepth() + 1},
+    m_name{name},
     m_localTransform{glm::mat4()},
-    m_globalTransform{parent->getWorldTransform()}
-    {
+    m_children{std::map<std::string, std::shared_ptr<Node>>{}} {
 
+  if (nullptr != parent) {
+    m_path = parent->getPath() + parent->getName();
+    m_depth = parent->getDepth() + 1;
+    m_globalTransform = parent->getWorldTransform();
 
-};
+  } else {
+    m_path = "";
+    m_depth = 0;
+    m_globalTransform = glm::mat4();
+  }
+}
 
 std::shared_ptr<Node> Node::getParent() {
-
   return std::shared_ptr<Node>();
 }
 
 void Node::setParent(std::shared_ptr<Node> node) {
   m_parent = node;
+  m_globalTransform = m_parent->getWorldTransform() * m_localTransform;
 }
 
-std::shared_ptr<Node> Node::getChild(const std::string &name) {
-  return std::shared_ptr<Node>();
+std::shared_ptr<Node> Node::getChild(std::string const& name) {
+  auto iter = m_children.find(name);
+
+  if (iter != m_children.end()) {
+    return iter->second;
+  }
+  return nullptr;
 }
 
-std::vector<std::shared_ptr<Node>> const &Node::getChildrenList() {
+std::map<std::string, std::shared_ptr<Node>> const &Node::getChildrenList() {
   return m_children;
 }
 
@@ -43,26 +54,46 @@ int Node::getDepth() {
 }
 
 glm::mat4 Node::getLocalTransform() {
-  return glm::mat4();
+  return m_localTransform;
 }
 
-void Node::setLocalTransform(const glm::mat4 &mat) {
+void Node::setLocalTransform(const glm::mat4 &newTransform) {
+  glm::mat4 relTransform = newTransform * glm::inverse(m_localTransform);
+  m_localTransform = newTransform;
 
+  for (auto& pair: m_children) {
+    pair.second->m_globalTransform *= relTransform;
+  }
 }
 
 glm::mat4 const &Node::getWorldTransform() {
   return m_globalTransform;
 }
 
-void Node::setWorldTransform(glm::mat4 const& mat) {
-  m_globalTransform
+void Node::setWorldTransform(glm::mat4 const& newTransform) {
+  glm::mat4 relTransform = newTransform * glm::inverse(m_globalTransform);
+  m_globalTransform = newTransform;
+
+  if (nullptr != m_parent) {
+    m_localTransform = newTransform * glm::inverse(m_parent->getWorldTransform());
+  }
+  for (auto &pair: m_children) {
+    pair.second->m_globalTransform *= relTransform;
+  }
 }
 
-void Node::addChildren(std::shared_ptr<Node>) {
-
+void Node::addChildren(std::shared_ptr<Node> child) {
+  m_children.emplace(child->getName(), child);
 }
 
 std::shared_ptr<Node> Node::removeChildren(const std::string &name) {
+  auto iter = m_children.find(name);
+
+  if (iter != m_children.end()) {
+    std::shared_ptr<Node> value = std::move(iter->second);
+    m_children.erase(iter);
+    return value;
+  }
   return nullptr;
 }
 
